e96ad0f933c0be0b1dd5fc60f67dd358550be625
0xkhan <0xkhan@protonmail.com>
2023-05-02 01:09:05 +0200

    Updata index.html and HolySheet.js

--- src/js/plugins/HolySheet.js
+++ src/js/plugins/HolySheet.js
@@ -1,16 +1,13 @@
 export default class HolySheet {
 
-    #data = {};
     #options;
     #sheetURL;
     #sheetBaseURL = 'https://docs.google.com/spreadsheets/d/';
-    // #sheetDataTypeJSON = '/gviz/tq?';
     #sheetDataTypeJSON = '/gviz/tq?tqx=out:json&headers=1';
 
     constructor(config) {
         this.#options = {
-            sheetId: "1tdRaVMG9BSry5SpJCH09VkslR1Sc7tPYH4b5m-RJn9c",
-            // sheetId: "1hxrpqGnVjs9efoq-eblYm8M3AHMDY9Q6vOihvHRxdZg",
+            sheetId: "",
             sheetTitle: "",
             sheetRange: "",
             dataType: 'json',
@@ -31,20 +28,23 @@ export default class HolySheet {
         this.setSheetURL();
     }
 
+    /**
+     * Sets the correct URL based on user input
+     */
     setSheetURL() {
         if (this.#options.sheetTitle.length !== 0 &&
             this.#options.sheetRange.length === 0) {
             this.#sheetURL  = this.#sheetBaseURL
                             + this.#options.sheetId
                             + this.#sheetDataTypeJSON
-                            + 'sheet='
+                            + '&sheet='
                             + this.#options.sheetTitle;
         } else if (this.#options.sheetTitle.length !== 0 &&
             this.#options.sheetRange.length !== 0) {
             this.#sheetURL  = this.#sheetBaseURL
                             + this.#options.sheetId
                             + this.#sheetDataTypeJSON
-                            + 'sheet='
+                            + '&sheet='
                             + this.#options.sheetTitle
                             + '&range='
                             + this.#options.sheetRange;
@@ -55,6 +55,11 @@ export default class HolySheet {
         }
     }
 
+    /**
+     * Fetches data from Google Sheets
+     *
+     * @return {Promise} data
+     */
     async fetchData() {
         const timeout = function(seconds) {
             return new Promise(function(_, reject) {
@@ -71,17 +76,70 @@ export default class HolySheet {
                 timeout(10)
             ]);
             const dataText = await response.text();
-            // const data = JSON.parse(dataText.substr(47).slice(0, -2));
             // const data = JSON.parse(dataText.slice(47, -2));
-            const data = JSON.parse(dataText.match(/(?<=.*\().*(?=\);)/s));
+            const dataJSON = JSON.parse(dataText.match(/(?<=.*\().*(?=\);)/s));
+            const data = this.#parseJSON(dataJSON);
             return data;
         } catch(err) {
             throw err;
         }
     }
 
-    getData() {
-        return this.#data;
+    /**
+     * Parser for converting raw JSON into readable Object
+     *
+     * @param {Object} data
+     * @return {Array} dataObjectsArray
+     */
+    #parseJSON(data) {
+        let cellData, propName, dataObject;
+        const dataObjectsArray = [];
+        const cols = data.table.cols;
+        const rows = data.table.rows;
+        const propNames = this.#convertPropNames(cols);
+
+        for (let i = 0; i < rows.length; i++) {
+            dataObject = {};
+
+            for (let j = 0; j < propNames.length; j++) {
+                propName = propNames[j];
+                cellData = rows[i]["c"][j];
+
+                if (cellData === null) {
+                    dataObject[propName] = "";
+                } else {
+                    dataObject[propName] = cellData["v"];
+                }
+            }
+
+            dataObjectsArray.push(dataObject);
+        }
+
+        return dataObjectsArray;
+    }
+
+    /**
+     * Converts property names into valid object keys
+     *
+     * @param {Array} props
+     * @return {Array} propsArray
+     */
+    #convertPropNames(props) {
+        let propsArray = [];
+        let prop, propPieces;
+
+        for (let i = 0; i < props.length; i++) {
+            prop = props[i].label.toLowerCase();
+            propPieces = prop.split(/[- ]+/);
+
+            propPieces = propPieces.map((prop, i) => {
+                return (i > 0) ? prop.charAt(0).toUpperCase() + prop.slice(1) : prop;
+            });
+
+            propsArray.push(propPieces.join(''));
+        }
+
+        return propsArray;
     }
 
 }
